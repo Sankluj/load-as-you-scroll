7db9fd54c82a394ebbaa7282a6d35dab66617b2b
Sankluj <matthieu.kluj@gmail.com>
2020-12-21 16:46:50 +0100

    chore(packaging): use rollup

---
+++ rollup.config.js
@@ -0,0 +1,18 @@
+import pkg from "./package.json";
+
+export default {
+  input: "src/index.js",
+  output: [
+    {
+      file: pkg.main,
+      format: "cjs",
+      exports: "auto",
+    },
+    {
+      file: pkg.module,
+      format: "es",
+      exports: "auto",
+    },
+  ],
+  external: ["react"],
+};
