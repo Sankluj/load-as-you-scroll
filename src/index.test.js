4306316d1097e7684565cacf4656843b338de5db
Sankluj <matthieu.kluj@gmail.com>
2020-12-22 01:23:26 +0100

    test(unit): test effect after observer intersection

--- src/index.test.js
+++ src/index.test.js
@@ -1,4 +1,4 @@
-import { render, screen } from "@testing-library/react";
+import { render, screen, act } from "@testing-library/react";
 import React, { Fragment } from "react";
 
 import useScrollStepper from "./index.js";
@@ -25,14 +25,21 @@ const Component = ({ elements }) => {
 };
 
 describe("useScrollStepper", () => {
+  let triggerObserver = null;
+
   beforeEach(() => {
     const observe = jest.fn();
     const disconnect = jest.fn();
 
-    window.IntersectionObserver = jest.fn(() => ({
-      observe,
-      disconnect,
-    }));
+    window.IntersectionObserver = jest.fn((handler) => {
+      triggerObserver = (isIntersecting) =>
+        handler([{ isIntersecting: true }], { disconnect });
+
+      return {
+        observe,
+        disconnect,
+      };
+    });
   });
 
   it("should throw an error if the first parameter is not an array", () => {
@@ -56,5 +63,18 @@ describe("useScrollStepper", () => {
     //THEN
     const displayedElts = screen.getAllByTestId("element");
     expect(displayedElts.length).toEqual(10);
+  });
+
+  it("should return a sub array of twenty elements after one intersection", () => {
+    //GIVEN
+    const elements = [...Array(5000)];
+
+    //WHEN
+    render(<Component elements={elements} />);
+    act(() => triggerObserver(true));
+
+    //THEN
+    const displayedElts = screen.getAllByTestId("element");
+    expect(displayedElts.length).toEqual(20);
   });
 });
